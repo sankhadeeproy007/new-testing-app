9aba1d44424ba4fb6b67f4684399aed89025416e
sankhadeeproy007 <sankhadeeproy007@gmail.com>
2020-11-29 12:02:00 +0530

    feat(): adds test for counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
-    <div className="App">
+    <div className="App" data-test="component-app">
       <h1>Testing react</h1>
+      <h2 data-test="counter-display">{count}</h2>
+      <button onClick={incrementCounter} data-test="increment-button">
+        Increment counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import Enzyme from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-import App from './App';
+import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders testing react text', () => {});
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+describe("checking the counter module", () => {
+  test("renders without error", () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent).toHaveLength(1);
+  });
+
+  test("should render increment button", () => {
+    const wrapper = setup();
+    const incrementButton = findByTestAttr(wrapper, "increment-button");
+    expect(incrementButton).toHaveLength(1);
+  });
+
+  test("should render count display", () => {
+    const wrapper = setup();
+    const countDisplay = findByTestAttr(wrapper, "counter-display");
+    expect(countDisplay).toHaveLength(1);
+  });
+
+  test("should start at 0", () => {
+    const wrapper = setup();
+    const countDisplay = findByTestAttr(wrapper, "counter-display").text();
+    expect(countDisplay).toBe("0");
+  });
+
+  test("should increment count on clicking button", () => {
+    const wrapper = setup();
+
+    const incrementButton = findByTestAttr(wrapper, "increment-button");
+
+    incrementButton.simulate("click");
+
+    const countDisplay = findByTestAttr(wrapper, "counter-display").text();
+
+    expect(countDisplay).toBe("1");
+  });
+});
