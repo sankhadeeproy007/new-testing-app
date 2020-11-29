fa9fe745dade70f8267f24d672f0ed597bb40dc3
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-11-29 10:04:37 +0530

    adds jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders testing react text', () => {});
