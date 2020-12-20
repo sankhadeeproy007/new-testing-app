fdd9417213f703be29da2c400a588d8fe2964255
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-12-20 20:50:24 +0530

    input component and test

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr } from '../../test/testutils';
 
 import Input from '.';
 
+const defaultProps = { secretWord: 'train' };
+
 const setup = (props = {}) => {
-  return shallow(<Input {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 describe('Input component', () => {
@@ -13,5 +17,21 @@ describe('Input component', () => {
     const wrapper = setup();
     const InputComponent = findByTestAttr(wrapper, 'input');
     expect(InputComponent).toHaveLength(1);
+  });
+
+  test('should update input filed based on change', () => {
+    // Set mock function
+    const mockSetCurrentGuess = jest.fn();
+
+    // Assign mock function to 2nd useState param
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+    const wrapper = setup();
+    const InputBox = findByTestAttr(wrapper, 'input-box');
+
+    // Set mock event and simulate onChange
+    const mockEvent = { target: { value: 'train' } };
+    InputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
   });
 });
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Input() {
-  return <div data-test="input" />;
+function Input({ secretWord }) {
+  console.log(secretWord);
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test="input">
+      <input
+        type="text"
+        value={currentGuess}
+        onChange={(e) => setCurrentGuess(e.target.value)}
+        data-test="input-box"
+      />
+      <button type="submit" data-test="submit-button">
+        Submit
+      </button>
+    </div>
+  );
 }
 
-Input.propTypes = {};
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
 
 export default Input;
