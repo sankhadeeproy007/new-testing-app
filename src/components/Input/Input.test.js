264abe3b767d3b042b9ea3edfc410120c8e82318
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-12-20 21:42:46 +0530

    Simulating submit click and tests

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -12,19 +12,25 @@ const setup = (props = {}) => {
   return shallow(<Input {...setupProps} />);
 };
 
-describe('Input component', () => {
-  test('should render without error', () => {
-    const wrapper = setup();
-    const InputComponent = findByTestAttr(wrapper, 'input');
-    expect(InputComponent).toHaveLength(1);
-  });
+test('should render without error', () => {
+  const wrapper = setup();
+  const InputComponent = findByTestAttr(wrapper, 'input');
+  expect(InputComponent).toHaveLength(1);
+});
+describe('Input component state changes', () => {
+  // Set mock function
+  let mockSetCurrentGuess = jest.fn();
 
-  test('should update input filed based on change', () => {
-    // Set mock function
-    const mockSetCurrentGuess = jest.fn();
+  // Mokcing setState function in beforeEach
+  beforeEach(() => {
+    // Clearing mock function
+    mockSetCurrentGuess.mockClear();
 
     // Assign mock function to 2nd useState param
     React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+  });
+
+  test('should update input filed based on change', () => {
     const wrapper = setup();
     const InputBox = findByTestAttr(wrapper, 'input-box');
 
@@ -33,5 +39,17 @@ describe('Input component', () => {
     InputBox.simulate('change', mockEvent);
 
     expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('should clear input after submit is clicked', () => {
+    const wrapper = setup();
+    const SubmitButton = findByTestAttr(wrapper, 'submit-button');
+
+    // In case we need to preventDefault
+    // SubmitButton.simulate('click', { preventDefault() {} });
+    // Simulate onClick
+    SubmitButton.simulate('click');
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
   });
 });
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ secretWord }) {
-  console.log(secretWord, 'secretWord');
   const [currentGuess, setCurrentGuess] = React.useState('');
+
+  const resetCurrentGuess = () => {
+    console.log(secretWord, 'secretWord');
+    setCurrentGuess('');
+  };
+
+  const handleGuessChange = (e) => {
+    setCurrentGuess(e.target.value);
+  };
+
   return (
     <div data-test="input">
       <input
         type="text"
         value={currentGuess}
-        onChange={(e) => setCurrentGuess(e.target.value)}
+        onChange={handleGuessChange}
         data-test="input-box"
       />
-      <button type="submit" data-test="submit-button">
+      <button
+        type="submit"
+        data-test="submit-button"
+        onClick={resetCurrentGuess}
+      >
         Submit
       </button>
     </div>
